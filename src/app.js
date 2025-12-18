e55b1b992e5aa7877a991ad78036aff0bc6e1d5f
LeonelAgustav <marcelinoagustav@gmail.com>
2025-12-18 23:31:44 +0700

    Final Backend MVP Release 1.0

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-a// ============================
+// ============================
 // API ROUTES
 // ============================
 app.use("/api/v1/auth", authRoutes);
