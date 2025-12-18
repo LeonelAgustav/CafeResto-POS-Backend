6a99c1b72d67da657cf94103a18bb6b3410d7a2f
LeonelAgustav <marcelinoagustav@gmail.com>
2025-12-18 20:35:02 +0700

    Final Backend MVP Release 1.0

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const inventoryRoutes = require('./routes/inventoryRoutes');
 const menuRoutes = require('./routes/menuRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +15,3 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/inventory', inventoryRoutes);
 app.use('/api/v1/menus', menuRoutes);
-
-app.listen(PORT, () => {
-    console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
-});
